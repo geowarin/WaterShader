b6796924a8a855a2b20d9b9880770ff81f0a2116
Geoffroy Warin <code@geowarin.com>
2025-05-12 01:32:36 +0200

    create a waterplane class

---
+++ Scenes/water_plane.gd
@@ -0,0 +1,28 @@
+@tool
+extends MeshInstance3D
+class_name WaterPlane
+
+var _mat: ShaderMaterial = preload("uid://c3mh5p2w5s8g7")
+var _normal: NoiseTexture2D = preload("uid://gq8n64t6y4go")
+var _wave_noise: NoiseTexture2D = preload("uid://dhkuuan5kku8y")
+
+
+func _ready() -> void:
+    if Engine.is_editor_hint() && mesh == null:
+        mesh = PlaneMesh.new()
+        mesh.size = Vcoector2(1, 1)
+        set_surface_override_material(0, _mat)
+        _mat.set_shader_parameter(&"wave", _wave_noise)
+        _mat.set_shader_parameter(&"texture_normal", _normal)
+        _mat.set_shader_parameter(&"texture_normal2", _normal)
+
+@export var size := Vector2i(1, 1):
+    set(value):
+        size = value
+        subdivize()
+
+func subdivize() -> void:
+    scale = Vector3(size.x, 1, size.y)
+    mesh.size = Vector2(1, 1)
+    mesh.subdivize_width = size.x * size.x
+    mesh.subdivize_depth = size.y * size.y
